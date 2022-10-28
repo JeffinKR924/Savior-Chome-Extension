781b570984cce07d79ea676532338fc0f16dc8b7
jkr59 <jkr59@drexel.edu>
2022-10-28 01:20:53 -0400

    Integrated redundancy checker function. Noted bugs and next steps

--- popup.js
+++ popup.js
@@ -3,6 +3,20 @@ let saveSession = document.getElementById("saveSession");
 let clearAll = document.getElementById("clearAll");
 var myDiv = document.getElementById("dynamicBtnDiv");
 
+function redundancyChecker(urlArray){
+  savedItems = parseInt(window.localStorage.length);
+  if (savedItems != 0) {
+    for (var i = 0; i < savedItems; i++){
+      storageKey = localStorage.key(i);
+      storageVal = (JSON.parse(window.localStorage.getItem(storageKey)))[0];
+      if (JSON.stringify(storageVal) == JSON.stringify(urlArray)){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function nameTrimmer(btnName) {
   if (btnName.length>19){
     btnName = btnName.slice(0, 19)+"...";
@@ -60,23 +74,6 @@ for (var i = 0; i < savedItems; i++){
         url: newUrl
       });
     }
-    // pageRenameFlag = 1;
-    // pageBtn.addEventListener('mousedown', (ev) => {
-    //   if (ev.which === 3) {
-    //     objectName = ev.currentTarget;
-    //     console.log(objectName);
-    //   }
-    // });
-    // pageBtn.addEventListener("mouseover", function( event ) {
-    //   hoverPageName = event.currentTarget.name;
-      // console.log(event.currentTarget);
-      // event.target.name, event.currentTarget.name, this.name?
-      // hoverPageName = event.currentTarget.name;
-      // How is this bottom code being called when i am not on a button?
-      // The only explanation is that it attaches to a button and doesnt call The
-      // top parent, just the bottom parent. so maybe if i add another check in
-      // the bottom part then that might work
-    // });
     myDiv.appendChild(pageBtn);
   }
   else {
@@ -106,36 +103,9 @@ for (var i = 0; i < savedItems; i++){
         });
       }
     }
-    // sessionRenameFlag = 1;
-    // sessionBtn.addEventListener('mousedown', (ev) => {
-    //   if (ev.which === 3) {
-    //     objectName = ev.currentTarget;
-    //     console.log(objectName);
-    //   }
-    // });
-    // chrome.contextMenus.onClicked.addListener((info, tab) => {
-    //   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    //     if (info.menuItemId === "deleteSession") {
-    //       window.localStorage.removeItem(this.name);
-    //       window.open('', '_blank').close();
-    //     }
-    //     else if (info.menuItemId === "rename" && sessionRenameFlag == 1) {
-    //       oldName = (JSON.parse(window.localStorage.getItem(hoverSessionName)));
-    //       let newSessionName = prompt("Enter a new name:", String(oldName[1]));
-    //       oldName[1] = newSessionName;
-    //       window.localStorage.setItem(String(hoverSessionName), JSON.stringify(oldName));
-    //       sessionRenameFlag+=1;
-    //       window.close();
-    //     }
-    //   });
-    // })
     myDiv.appendChild(sessionBtn);
   }
 }
-
-// The hover portion of this code saves this buttons name into session storage
-// If I have the button name, then maybe i can use that to identify which button
-// it is, and then change the innerhtml of that button?
 
 save.onclick = function (element) {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
@@ -143,6 +113,7 @@ save.onclick = function (element) {
     urlArray = [];
     url = tabs[0].url;
     urlArray.push(url);
+    redun = redundancyChecker(urlArray);
     valueArray.push(urlArray);
     favIconURL = "chrome://favicon/size/23@1x/" + url;
     var favIconImage = document.createElement('img');
@@ -154,9 +125,9 @@ save.onclick = function (element) {
     currentUrl = url;
     currentName = btnNamer(currentUrl);
     valueArray.push(currentName);
-    window.localStorage.setItem(String(url), JSON.stringify(valueArray));
-    window.sessionStorage.setItem('currentPage', String(url));
-    // maybe if i can save url as a global var somehow that would fix the current issue?
+    if (redun == false) {
+      window.localStorage.setItem(String(url), JSON.stringify(valueArray));
+    }
     btn = document.createElement("BUTTON");
     btn.className = 'dynamicButton';
     btn.innerHTML = (currentName);
@@ -183,11 +154,14 @@ saveSession.onclick = function (element) {
         url = tabs[i].url;
         urlArray.push(url);
       }
+      redun = redundancyChecker(urlArray);
       valueArray.push(urlArray);
       localStorageLength = (localStorage.length)+1;
       valueArray.push("Session " + String(localStorageLength));
       var arrayName = ("SESSION924"+urlArray.toString());
-      window.localStorage.setItem(String(arrayName), JSON.stringify(valueArray));
+      if (redun == false) {
+        window.localStorage.setItem(String(arrayName), JSON.stringify(valueArray));
+      }
       btn = document.createElement("BUTTON");
       btn.className = 'dynamicButton';
       btn.innerHTML = ("New Session");
@@ -215,7 +189,7 @@ saveSession.onclick = function (element) {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     if (info.menuItemId === "delete") {
-      if (objectName!= null) {
+      if (objectName != '') {
         window.localStorage.removeItem(objectName);
         window.open('', '_blank').close();
       }
@@ -223,7 +197,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     else if (info.menuItemId === "rename") {
       if (objectName!= '') {
         oldName = (JSON.parse(window.localStorage.getItem(objectName)));
-        // console.log(oldName);
         let newName = prompt("Enter a new name:", String(oldName[1]));
         oldName[1] = newName;
         window.localStorage.setItem(String(objectName), JSON.stringify(oldName));
@@ -263,8 +236,6 @@ window.addEventListener('mousedown', (event) => {
   if (event.which === 3) {
     divTest = String(event.target.className); 
     obj = event.target;
-    // console.log(divTest);
-    // console.log(obj);
     if (divTest == 'fa-solid fa-file' || divTest == 'favIcon' || divTest == 'fa fa-folder') {
       while(divTest != 'dynamicButton'){
         obj = obj.parentElement;
@@ -281,23 +252,15 @@ window.addEventListener('mousedown', (event) => {
   }
 });
 
-// Fix it so you can rename right after button is saved and page hasnt closed
+// Deleting button when it is in just saved mode has not been created yet
+// BUG: When you save a button and a session at the same time and you are still in
+// just saved mode, the renaming functionality bugs out. It only lets you rename
+// the last saved item
+// TODO FIRST: Reread the rename functionality to defintively understand what you did
 
 
 
-// this.name seems to be working with rename functionality really well. I have not seen
-// any issues with that portion yet. this.name, currentTarget, and current have issues
-// with the delete page functionality. Have to find a reliable fix for that. Also, gotta
-// find a fix for that weird delay that causes you to hover over another button after
-// deleting page. Possible solution for this could be that it saves the last hovered button
-// name constantly and then uses other if statements to prevent deletion when you stop hovering
-// and press the delete button? Also, the session names face a serious bug rn with the naming
+//  Also, the session names face a serious bug rn with the naming
 // scheme. They can overlap if one button is deleted, since they are based on current length.
 // Can fix this issue by having a num for session that constantly goes up despite the length,
 // like a counter.
-
-// The deletion works now. But now the issue is that if you right click on the button and
-// then right click on anywhere that is not a dynamic button and press delete, it deletes
-// the last right clicked button. I am thinking that if I add another window event listener
-// that generally checks to make sure that you arent right clicking the window or the save
-// utility buttons, then that could work maybe.
